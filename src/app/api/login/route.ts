899e7c448e4ea43c699211bd116636fb8299b591
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-01-10 22:23:31 +0000

    feat(api): add login route

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,21 @@
+import { Team } from "@/app/models/Team";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ error: "Bad Request" });
+  }
+
+  try {
+    const token = await Team.login(email, password);
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+}
