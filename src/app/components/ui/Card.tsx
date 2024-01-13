e147e9bddb97b095e2c953fca09d10d7d5ebab98
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-01-13 16:26:13 +0000

    feat(ui): add card

---
+++ src/app/components/ui/Card.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+interface Props {
+  className: string;
+  children: ReactNode;
+}
+
+const Card = (props: Props) => {
+  return (
+    <div
+      className={`rounded-lg border bg-card text-card-foreground shadow-sm p-6 ${props.className}`}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
