11038127e428642bb284865856cab6c48ba5dd17
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-02-01 22:40:05 +0000

    feat: add delete route

---
+++ src/app/api/sprint/delete/route.ts
@@ -0,0 +1,25 @@
+import { withAuth } from "@/app/lib/auth/withAuth";
+import { SprintHandler } from "@/app/models/Sprint";
+import { TeamSettings } from "@/app/models/Team";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function POST(req: NextRequest) {
+  return await withAuth(req, async (team: TeamSettings) => {
+    const sprintHandler = new SprintHandler();
+    const { name } = await req.json();
+    if (!team._id || !name) {
+      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
+    }
+
+    try {
+      await sprintHandler.delete(team._id, name);
+      return NextResponse.json({}, { status: 201 });
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json(
+        { error: "Unable to delete sprint" },
+        { status: 500 }
+      );
+    }
+  });
+}
