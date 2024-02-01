96bb953b3331381ada575d4bf7b05975023c474b
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-02-01 23:26:09 +0000

    feat: add selected row style

--- src/app/components/SprintHistory/SprintHistoryCell.tsx
+++ src/app/components/SprintHistory/SprintHistoryCell.tsx
@@ -2,13 +2,16 @@ import { ReactNode } from "react";
 
 interface Props {
   className?: string;
+  selected?: boolean;
   children?: ReactNode;
 }
 
 const SprintHistoryCell = (props: Props) => {
   return (
     <div
-      className={`h-10 flex items-center px-4 py-2 text-nowrap bg-white ${props.className}`}
+      className={`h-10 flex items-center px-4 py-2 text-nowrap ${
+        props.selected ? "bg-slate-100" : "bg-white"
+      }  ${props.className}`}
     >
       {props.children}
     </div>
