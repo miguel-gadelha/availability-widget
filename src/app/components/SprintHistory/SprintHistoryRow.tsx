211dbf923d61e0fe2feb8395e73d11e82feb60eb
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-02-05 23:35:34 +0000

    feat: fix sprint row state

--- src/app/components/SprintHistory/SprintHistoryRow.tsx
+++ src/app/components/SprintHistory/SprintHistoryRow.tsx
@@ -1,32 +1,63 @@
+"use client";
+
 import { Sprint } from "@/types";
 import SprintHistoryCell from "./SprintHistoryCell";
 import Link from "next/link";
+import { ChangeEventHandler, useEffect, useRef, useState } from "react";
+import RadioButton from "../ui/RadioButton";
 
 interface Prop {
   sprint: Sprint;
   className?: string;
+  selected: boolean;
+  onSelected: () => void;
 }
 
-const SprintHistoryRow = (props: Prop) => {
+const SprintHistoryRow = ({
+  sprint,
+  className,
+  selected,
+  onSelected,
+}: Prop) => {
+  const [selectedRow, setSelectedRow] = useState(selected);
+
+  useEffect(() => {
+    setSelectedRow(selected);
+  }, [selected]);
+
+  const handleSelectionChange = (checked: boolean) => {
+    onSelected();
+  };
+
   return (
     <>
       <div className="col-span-3">
-        <SprintHistoryCell className={props.className}>
-          {
-            <Link href={`/widget/${props.sprint.teamId}/${props.sprint.name}`}>
-              {decodeURI(props.sprint.name)}
-            </Link>
-          }
+        <SprintHistoryCell className={className} selected={selectedRow}>
+          <RadioButton
+            checked={selectedRow}
+            onChange={handleSelectionChange}
+            className="mr-2"
+          ></RadioButton>
+
+          <Link href={`/widget/${sprint.teamId}/${sprint.name}`}>
+            {decodeURI(sprint.name)}
+          </Link>
         </SprintHistoryCell>
       </div>
       <div className="col-start-4">
-        <SprintHistoryCell className={props.className + " flex justify-end"}>
-          {props.sprint.length}
+        <SprintHistoryCell
+          className={className + " flex justify-end"}
+          selected={selectedRow}
+        >
+          {sprint.length}
         </SprintHistoryCell>
       </div>
       <div className="col-start-5">
-        <SprintHistoryCell className={props.className + " flex justify-end"}>
-          {Number(props.sprint.availability) + "%"}
+        <SprintHistoryCell
+          className={className + " flex justify-end"}
+          selected={selectedRow}
+        >
+          {Number(sprint.availability) + "%"}
         </SprintHistoryCell>
       </div>
     </>
