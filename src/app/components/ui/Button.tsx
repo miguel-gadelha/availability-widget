3badb305fc06a7990201d06712bb8488929692d4
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-02-06 23:12:11 +0000

    feat: add button variants

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -6,12 +6,28 @@ interface Props {
   children?: ReactNode;
   disabled?: boolean;
   onClick?: () => void;
+  variant?: "destructive" | "secondary";
 }
 const Button = (props: Props) => {
+  const defaultClass = " bg-slate-900 hover:bg-slate-800 text-white";
+  const destructiveClass = "bg-red-500 text-white hover:bg-red-500/90";
+  const secondaryClass = "bg-slate-200 text-slate-900 hover:bg-slate-200/80";
+
+  const getVariantClasses = () => {
+    if (props.variant === "destructive") {
+      return destructiveClass;
+    } else if (props.variant === "secondary") {
+      return secondaryClass;
+    }
+
+    return defaultClass;
+  };
   return (
     <button
       type={props.type}
-      className={`rounded-md px-4 py-2 bg-slate-900 hover:bg-slate-800 disabled:bg-gray-400 text-sm font-medium leading-none text-white ${props.className}`}
+      className={`rounded-md px-4 py-2 text-sm font-medium leading-nonedisabled:bg-gray-400   ${getVariantClasses()} ${
+        props.className
+      }`}
       onClick={props.onClick}
       disabled={props.disabled}
     >
