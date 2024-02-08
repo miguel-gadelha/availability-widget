dbee54507ca7c4774fbf9b91920bd0ccf328a358
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-02-08 23:23:10 +0000

    feat: add loading to button

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Spinner from "./Spinner";
 
 interface Props {
   className?: string;
@@ -7,16 +8,25 @@ interface Props {
   disabled?: boolean;
   onClick?: () => void;
   variant?: "destructive" | "secondary";
+  isLoading?: boolean;
 }
-const Button = (props: Props) => {
+const Button = ({
+  type,
+  className,
+  onClick,
+  disabled,
+  children,
+  variant,
+  isLoading,
+}: Props) => {
   const defaultClass = " bg-slate-900 hover:bg-slate-800 text-white";
   const destructiveClass = "bg-red-500 text-white hover:bg-red-500/90";
   const secondaryClass = "bg-slate-200 text-slate-900 hover:bg-slate-200/80";
 
   const getVariantClasses = () => {
-    if (props.variant === "destructive") {
+    if (variant === "destructive") {
       return destructiveClass;
-    } else if (props.variant === "secondary") {
+    } else if (variant === "secondary") {
       return secondaryClass;
     }
 
@@ -24,14 +34,13 @@ const Button = (props: Props) => {
   };
   return (
     <button
-      type={props.type}
-      className={`rounded-md px-4 py-2 text-sm font-medium leading-nonedisabled:bg-gray-400   ${getVariantClasses()} ${
-        props.className
-      }`}
-      onClick={props.onClick}
-      disabled={props.disabled}
+      type={type}
+      className={`rounded-md px-4 py-2 text-sm font-medium leading-nonedisabled:bg-gray-400 flex items-center justify-center ${getVariantClasses()} ${className}`}
+      onClick={onClick}
+      disabled={disabled}
     >
-      {props.children}
+      {children}
+      {isLoading && <Spinner className="ml-3" />}
     </button>
   );
 };
