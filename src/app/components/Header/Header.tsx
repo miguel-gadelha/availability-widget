5d7c9896437a2f7057c745e10b67abd2d96cd07e
Miguel Gadelha <mb.gadelha18@gmail.com>
2024-02-11 23:35:47 +0000

    feat: add Header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { TeamContext } from "@/app/context/TeamContext";
+import { useContext } from "react";
+import SmallButton from "../ui/SmallButton";
+import { LogOut, UserCog } from "lucide-react";
+import LogOutButton from "./LogOutButton";
+interface Props {
+  className?: string;
+}
+
+const Header = ({ className }: Props) => {
+  const context = useContext(TeamContext);
+
+  if (!context) {
+    return null;
+  }
+
+  return (
+    <section
+      className={`flex items-center justify-center gap-10 self-stretch bg-slate-800 py-6 ${className}`}
+    >
+      <div className="w-2/3 flex justify-between items-center">
+        <h1 className="text-slate-50 text-2xl font-semibold leading-none">
+          {context.name}
+        </h1>
+        <nav className="flex">
+          <SmallButton className="mr-6">
+            <UserCog width={16} height={16} />
+          </SmallButton>
+
+          <LogOutButton />
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,5 +1,14 @@
+import Header from "../components/Header/Header";
 import SprintHistory from "../components/SprintHistory/SprintHistory";
+import TeamContextProvider from "../context/TeamContext";
 
 export default function Page() {
-  return <SprintHistory />;
+  return (
+    <section className="flex flex-col w-full h-screen items-center">
+      <TeamContextProvider>
+        <Header className="mb-12" />
+      </TeamContextProvider>
+      <SprintHistory />
+    </section>
+  );
 }
